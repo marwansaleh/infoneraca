8194d1bd81495eb512ddafa1eb1ca330f6c3d693
marwansaleh <marwansaleh@192.168.1.100>
2016-03-27 13:15:09 +0700

    Update config

--- engine/application/views/components/_mainmenu.php
+++ engine/application/views/components/_mainmenu.php
@@ -2,7 +2,7 @@
     <ul id="mainmenu" class="nav nav-justified">
         <li <?php echo $active_menu=='home'?'class="active"':''; ?>>
             <a href="<?php echo site_url(); ?>">
-                <img src="<?php echo site_url('assets/img/one.png'); ?>" >
+                <span class="glyphicon glyphicon-home"></span>
             </a>
         </li>
         <?php if (isset($mainmenus)): ?>
