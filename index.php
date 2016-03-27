62ba1d26bf99334ca699d9b9222fd9499deafb03
marwansaleh <marwansaleh@192.168.1.100>
2016-03-27 11:56:53 +0700

    Update config

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('Asia/Jakarta');
 
 $path = dirname(__FILE__);
+echo $path; exit;
 $testing = '/home/sloki/user/k2427808/sites/pre.indonesiasatu.co/www';
 $production_1 = '/home/sloki/user/k2427808/sites/indonesiasatu.co/www';
 $production_2 = '/home/sloki/user/k2427808/sites/www.indonesiasatu.co/www';
